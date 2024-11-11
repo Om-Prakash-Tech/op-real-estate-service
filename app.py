e5a4fc8ff8531421c763fdd5e9af538189a3977f
Yash-231 <yashsingalkar@gmail.com>
2024-11-11 14:59:10 +0530

    vercel deployed

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from api_v1.routes.transactions import transactions_blueprint
 from api_v1.routes.metrics import metrics_blueprint
 from api_v1.routes.deals import deals_blueprint
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-cors = CORS(application)
+cors = CORS(app)
 
 api_v1_blueprint = Blueprint('api_v1', __name__)
 
-@application.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return make_response()
 
@@ -26,7 +26,7 @@ api_v1_blueprint.register_blueprint(transactions_blueprint, url_prefix='/transac
 api_v1_blueprint.register_blueprint(metrics_blueprint, url_prefix='/metrics')
 api_v1_blueprint.register_blueprint(deals_blueprint, url_prefix='/deals')
 
-application.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
+app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
 
 if __name__ == "__main__":
-    application.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=False)
