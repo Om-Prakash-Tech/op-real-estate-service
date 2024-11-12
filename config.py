a51817319a92c3ac2b8511df3ba5edf7fea80b3f
Yash-231 <yashsingalkar@gmail.com>
2024-11-12 18:49:30 +0530

    updated service account info

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 FLASK_ENV = os.getenv('FLASK_ENV')
 service_account_info = json.loads(os.getenv('SERVICE_ACCOUNT_KEY'))
-print('real-estate-firebase-adminsdk.json' if FLASK_ENV == 'localhost' else service_account_info)
-cred = credentials.Certificate('real-estate-firebase-adminsdk.json' if FLASK_ENV == 'localhost' else service_account_info)
+
+cred = credentials.Certificate(service_account_info)
 initialize_app(cred)
 
 db = firestore.client()
