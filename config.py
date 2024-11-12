60785d431d8496db0d4480b6404fad2c1782f090
Yash-231 <yashsingalkar@gmail.com>
2024-11-12 19:19:41 +0530

    config changes

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import os
 from dotenv import load_dotenv
 from firebase_admin import credentials, firestore, initialize_app
+from google.oauth2 import service_account
+import base64
 import json
 load_dotenv()
 
 FLASK_ENV = os.getenv('FLASK_ENV')
-service_account_info = json.loads(os.getenv('SERVICE_ACCOUNT_KEY'))
+service_key_b64 = os.getenv('SERVICE_ACCOUNT_KEY')
 
-cred = credentials.Certificate(service_account_info)
+service_key_json = base64.b64decode(service_key_b64).decode('utf-8')
+service_account_info = json.loads(service_key_json)
+
+cred = service_account.Credentials.from_service_account_info(service_account_info)
+# cred = credentials.Certificate('real-estate-firebase-adminsdk.json')
 initialize_app(cred)
 
 db = firestore.client()
