fc87471b6918e7e3cbfb839b7a1c1f24ff7695ea
Yash-231 <yashsingalkar@gmail.com>
2024-11-12 18:45:11 +0530

    changed firebase config

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 from firebase_admin import credentials, firestore, initialize_app
+import json
 load_dotenv()
 
 FLASK_ENV = os.getenv('FLASK_ENV')
-
-cred = credentials.Certificate('real-estate-firebase-adminsdk.json')
+service_account_info = json.loads(os.getenv('SERVICE_ACCOUNT_KEY'))
+print('real-estate-firebase-adminsdk.json' if FLASK_ENV == 'localhost' else service_account_info)
+cred = credentials.Certificate('real-estate-firebase-adminsdk.json' if FLASK_ENV == 'localhost' else service_account_info)
 initialize_app(cred)
 
 db = firestore.client()
